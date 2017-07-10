f2ebcc4860a6330bed50da3152dfa6ad4f89fa28
yulia <julie_gal@mail.ru>
2017-07-10 21:15:03 +0500

    отредактировала урлы

--- catalogue/urls.py
+++ catalogue/urls.py
@@ -4,7 +4,7 @@ from .views import ProductListView, AddProductsView, ProductDetailView
 
 urlpatterns = [
 url(r'^(?:(?P<subcat>\d+)/)?$', ProductListView.as_view(), name='home'),
-url(r'add-product', AddProductsView.as_view(), name='add-product'),
-url(r'^product/(?P<pk>\d+)/$', ProductDetailView.as_view(),
+url(r'^add/', AddProductsView.as_view(), name='add-product'),
+url(r'^detail/(?P<pk>\d+)/$', ProductDetailView.as_view(),
     name='product_detail'),
 ]
