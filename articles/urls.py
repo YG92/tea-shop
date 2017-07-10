18c4c3ee08951a2657818f74e9be05b9a126897c
yulia <julie_gal@mail.ru>
2017-07-10 21:10:55 +0500

    добавила урлы для корзины

--- articles/urls.py
+++ articles/urls.py
@@ -3,8 +3,8 @@ from .import views
 from .views import ArticleListView, ArticleDetailView, AddArticleView
 
 urlpatterns = [
-url(r'^article-detail/(?:(?P<pk>\d+)/)?$', ArticleDetailView.as_view(),
+url(r'^detail/(?:(?P<pk>\d+)/)?$', ArticleDetailView.as_view(),
     name='article-detail'),
-url(r'^article-list$', ArticleListView.as_view(), name='article_list'),
-url(r'^new-article$', AddArticleView.as_view(), name='new_article'),
+url(r'^list/$', ArticleListView.as_view(), name='article_list'),
+url(r'^new/$', AddArticleView.as_view(), name='new_article'),
 ]
