34b0f6067a4590ad26bda0be3dd948d3f37d5fbf
yulia Galiulina <julie_gal@mail.ru>
2017-07-14 23:43:31 +0500

    вью для обработки заказа

--- order/views.py
+++ order/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from .models import OrderItem
+from django.views.generic import CreateView
+from .forms import OrderCreateForm
+from cart.cart import Cart
 
-# Create your views here.
+class OrderCreateView(CreateView):
+    cart = Cart(request)
+    form_class = OrderCreateForm
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            for item in self.cart:
+                OrderItem.objects.create(order=order,
+                                        product=item['product'],
+                                        price=item['price'],
+                                        quantity=item['quantity'])
+            self.cart.clear()
+            return HttpResponse('Спасибо за покупку')
+        else:
+            return self.form_invalid(form)
