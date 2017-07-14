b9f6e7089b4db9fa031b80389afc364696040648
yulia Galiulina <julie_gal@mail.ru>
2017-07-14 18:50:29 +0500

    удалила ненужные переменные, добавила возвращение товара при удалении из корзины

--- cart/cart.py
+++ cart/cart.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from catalogue.models import Product
 
+
 class Cart():
 
     def __init__(self, request):
@@ -10,15 +11,15 @@ class Cart():
             cart = self.session[settings.CART_SESSION_ID] = {}
         self.cart = cart
 
-    def add(self, product, quantity=1, update_quantity=False):
+    def add(self, product, quantity=1):
         product_id = str(product.id)
         if product_id not in self.cart:
-            self.cart[product_id] = {'quantity': 0,
+            self.cart[product_id] = {'quantity': quantity,
                                      'price': str(product.price)}
-        if update_quantity:
-            self.cart[product_id]['quantity'] = quantity
         else:
             self.cart[product_id]['quantity'] += quantity
+        product.in_stock -= quantity
+        product.save()
         self.save()
 
     def save(self):
@@ -28,6 +29,8 @@ class Cart():
     def remove(self, product):
         product_id = str(product.id)
         if product_id in self.cart:
+            product.in_stock += self.cart[product_id]['quantity']
+            product.save()
             del self.cart[product_id]
             self.save()
 
@@ -41,7 +44,7 @@ class Cart():
             item['total_price'] = int(item['price']) * item['quantity']
             yield item
 
-    def __len__(self):
+    def get_total_quantity(self):
         return sum(item['quantity'] for item in self.cart.values())
 
     def get_total_price(self):
