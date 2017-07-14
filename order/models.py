c0a12fcd1c87308bfea406c172351c9cd68cd184
yulia Galiulina <julie_gal@mail.ru>
2017-07-14 23:16:02 +0500

    модель заказа

--- order/models.py
+++ order/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from catalogue.models import Product
 
-# Create your models here.
+class Order(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    city = models.CharField(max_length=50)
+    address = models.CharField(max_length=50)
+
+    def get_total_cost(self):
+        return sum(item.get_cost() for item in self.items.all())
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order)
+    product = models.ForeignKey(Product)
+    price = models.PositiveIntegerField()
+    quantity = models.PositiveIntegerField()
+
+    def get_cost():
+        return self.price * self.quantity
