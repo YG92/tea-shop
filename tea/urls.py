9ba5d1c37e56e6765d9f78d81ac1dc44055e6f64
yulia Galiulina <julie_gal@mail.ru>
2017-07-17 12:13:42 +0500

    добавила нэймспэйс заказа

--- tea/urls.py
+++ tea/urls.py
@@ -2,13 +2,13 @@ from django.conf import settings
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.conf.urls.static import static
-from django.contrib.auth.views import login, logout
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^account/', include('account.urls')),
     url(r'^product/', include('catalogue.urls')),
     url(r'^cart/', include('cart.urls', namespace='cart')),
+    url(r'^order/', include('order.urls', namespace='order')),
     url(r'^article/', include('articles.urls')),
-    url(r'^login/', login, {'template_name': 'login.html'}, name='login'),
-    url(r'^logout/', logout, name='logout'),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
