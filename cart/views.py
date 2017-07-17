3762195d4b5707ad9274d2491ee642cad91bba73
yulia Galiulina <julie_gal@mail.ru>
2017-07-17 12:23:47 +0500

    функция удаления товара

--- cart/views.py
+++ cart/views.py
@@ -2,30 +2,24 @@ from .cart import Cart
 from .forms import CartAddProductForm
 from catalogue.models import Product
 from django.shortcuts import resolve_url, redirect, get_object_or_404
-from django.views.generic import FormView, TemplateView, UpdateView
+from django.views.generic import FormView, TemplateView
 
-class CartAddFormView(TemplateView):
+
+class CartAddFormView(FormView):
     form_class = CartAddProductForm
     template_name = 'cart_add.html'
-    success_url = '/product/'
-    product = None
-
 
     def post(self, request, *args, **kwargs):
         form = self.form_class(request.POST)
-        cart = Cart(request)
         product = get_object_or_404(Product, id=kwargs['id'])
-        if form.is_valid():
-            cd = form.cleaned_data
-            cart.add(product=product, quantity=cd['quantity'],
-                    update_quantity=cd['update'])
-            return redirect ('home')
+        if form.is_valid(request, **kwargs):
+            form.cart_add(request, *args, **kwargs)
+            return redirect ('/cart/')
         else:
-            return super(CartAddFormView, self).get(request, *args, **kwargs)
+            return self.form_invalid(form)
 
 
-class CartDetailView(FormView):
-    form_class = CartAddProductForm
+class CartDetailView(TemplateView):
     template_name = 'cart_detail.html'
 
     def get_context_data(self, **kwargs):
@@ -33,5 +27,9 @@ class CartDetailView(FormView):
         context["cart"] = Cart(self.request)
         return context
 
-    def form_valid(self, form):
-        return super(CartDetailFormView, self).form_valid(form)
+
+def Remove(request, **kwargs):
+    cart = Cart(request)
+    product = get_object_or_404(Product, id=kwargs['id'])
+    cart.remove(product)
+    return redirect (request.META.get('HTTP_REFERER'))
