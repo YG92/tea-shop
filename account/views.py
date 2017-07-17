9c018a3328b975867dd6a85568b29a8d8213f967
yulia Galiulina <julie_gal@mail.ru>
2017-07-17 15:09:06 +0500

    удалила ненужные импорты

--- account/views.py
+++ account/views.py
@@ -1,7 +1,6 @@
 from django.views.generic.edit import CreateView
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
-from django.shortcuts import redirect
+
 
 class RegisterFormView(CreateView):
     form_class = UserCreationForm
