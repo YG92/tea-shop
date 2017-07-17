461e527f46365bfcba4cd305938143774d08087a
yulia Galiulina <julie_gal@mail.ru>
2017-07-17 12:19:41 +0500

    валидация формы, метод добавления товара

--- cart/forms.py
+++ cart/forms.py
@@ -1,9 +1,31 @@
 from django import forms
+from .cart import Cart
+from catalogue.models import Product
+from django.shortcuts import get_object_or_404
+from django.core.exceptions import ValidationError
 
-PRODUCT_QUANTITY_CHOICES = [(i, str(i)) for i in range(1, 21)]
 
 class CartAddProductForm(forms.Form):
-    quantity = forms.TypedChoiceField(choices=PRODUCT_QUANTITY_CHOICES, coerce=int)
-    update = forms.BooleanField(required=False, initial=False, widget=forms.HiddenInput)
 
-    
+    quantity = forms.IntegerField(initial=1,
+                                min_value=1,
+                                label='Количество:')
+
+
+    def is_valid(self, request, **kwargs):
+        valid = super(CartAddProductForm, self).is_valid()
+        self.product = get_object_or_404(Product, id=kwargs['id'])
+        quantity = request.POST.get('quantity')
+        in_stock = self.product.in_stock
+        if int(quantity) <= in_stock:
+            return True
+        else:
+            self.errors['Неверное количество:'] = '''
+                        Всего доступно {} шт.'''.format(in_stock)
+            return False
+
+
+    def cart_add(self, request, **kwargs):
+        cart = Cart(request)
+        cd = self.cleaned_data
+        cart.add(product=self.product, quantity=cd['quantity'])
