cff7a589f8d68ae6181ab01a617ef73689c50c8d
yulia Galiulina <julie_gal@mail.ru>
2017-07-17 13:03:55 +0500

    добавила лейблы для формы заказа

--- order/forms.py
+++ order/forms.py
@@ -4,7 +4,14 @@ from .models import Order
 class OrderCreateForm(forms.ModelForm):
     class Meta:
         model = Order
-        fields = ['first_name', 
+        fields = ['first_name',
                 'last_name',
                 'city',
                 'address']
+
+        labels = {
+                'first_name' : ('Фамилия'),
+                'last_name': ('Имя'),
+                'city': ('Город'),
+                'address': ('Адрес')
+        }
