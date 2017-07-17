7eed299b793f27ba4895be1872d08a4f54eac361
yulia Galiulina <julie_gal@mail.ru>
2017-07-17 13:27:37 +0500

    сохранение заказа

--- order/views.py
+++ order/views.py
@@ -4,19 +4,27 @@ from django.views.generic import CreateView
 from .forms import OrderCreateForm
 from cart.cart import Cart
 
+
 class OrderCreateView(CreateView):
-    cart = Cart(request)
     form_class = OrderCreateForm
+    template_name = 'create_order.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(OrderCreateView, self).get_context_data(**kwargs)
+        context["cart"] = Cart(self.request)
+        return context
 
     def post(self, request, *args, **kwargs):
+        cart = Cart(request)
         form = self.form_class(request.POST)
         if form.is_valid():
-            for item in self.cart:
+            order = form.save()
+            for item in cart:
                 OrderItem.objects.create(order=order,
                                         product=item['product'],
                                         price=item['price'],
                                         quantity=item['quantity'])
-            self.cart.clear()
+            cart.clear()
             return HttpResponse('Спасибо за покупку')
         else:
             return self.form_invalid(form)
