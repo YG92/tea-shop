8c8c42456d08686fdfc29b213084ca3ccb6f96b3
yulia Galiulina <julie_gal@mail.ru>
2017-07-17 15:54:48 +0500

    картинки в медиа

--- articles/models.py
+++ articles/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Article(models.Model):
     title = models.CharField(max_length=200)
-    image = models.ImageField(upload_to='media')
+    image = models.ImageField(upload_to='')
     text = models.TextField(blank=True)
     added_at = models.DateTimeField(blank=True, auto_now_add=True)
 
