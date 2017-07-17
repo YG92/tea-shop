ca9ece3552e479a07b7117358ef758fe70e5c5d3
yulia Galiulina <julie_gal@mail.ru>
2017-07-17 12:21:11 +0500

    урл удаления товара

--- cart/urls.py
+++ cart/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
 from .import views
-from .views import CartAddFormView, CartDetailView
+from .views import Remove, CartAddFormView, CartDetailView#, CartRemoveView
 
 urlpatterns = [
 url(r'^add/(?P<id>\d+)/$', CartAddFormView.as_view(), name='cart-add'),
-url(r'^detail/', CartDetailView.as_view(), name='cart-detail'),
+url(r'^remove/(?P<id>\d+)/$', views.Remove, name='cart-remove'),
+url(r'^$', CartDetailView.as_view(), name='cart-detail'),
 ]
