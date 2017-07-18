d5873d5d7a354db0eff97a9d835465ca0dfd954b
yulia Galiulina <julie_gal@mail.ru>
2017-07-18 17:20:29 +0500

    урл - спасибо за покупку

--- order/urls.py
+++ order/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
 from .import views
-from .views import OrderCreateView
+from .views import OrderCreateView, ThanksView
 
 urlpatterns = [
 url(r'^create/$', OrderCreateView.as_view(), name='order'),
+url(r'^thanks/$', ThanksView.as_view(), name='thanks'),
 ]
