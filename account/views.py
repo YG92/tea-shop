66fd59e4f82575dfa313699e86c15b0ddeb1ede7
yulia Galiulina <julie_gal@mail.ru>
2017-07-19 17:44:05 +0500

    юзер автоматически авторизуется и входит после регистрации

--- account/views.py
+++ account/views.py
@@ -1,8 +1,18 @@
-from django.views.generic.edit import CreateView
+from django.shortcuts import redirect
+from django.views.generic import CreateView, TemplateView
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login
 
 
 class RegisterFormView(CreateView):
     form_class = UserCreationForm
-    success_url = '/account/login/'
     template_name = 'register.html'
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            form.save()
+            new_user = authenticate(username=form.cleaned_data['username'],
+                                    password=form.cleaned_data['password1'])
+        login(self.request, new_user)
+        return redirect('/order/profile')
