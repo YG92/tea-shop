a92a06c587a6633cbfb93d74ca5edd509c505cb4
yulia Galiulina <julie_gal@mail.ru>
2017-07-19 18:11:07 +0500

    ссылка для личного кабинета

--- order/urls.py
+++ order/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
 from .import views
-from .views import OrderCreateView, ThanksView
+from .views import OrderCreateView, OrderListView, ThanksView
 
 urlpatterns = [
 url(r'^create/$', OrderCreateView.as_view(), name='order'),
+url(r'^profile/$', OrderListView.as_view(), name='profile'),
 url(r'^thanks/$', ThanksView.as_view(), name='thanks'),
 ]
