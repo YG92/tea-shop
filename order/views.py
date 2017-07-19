bc598a96e3aa9acb9486c3b05055410cddaf2dca
yulia Galiulina <julie_gal@mail.ru>
2017-07-19 18:10:17 +0500

    вью для списка заказов

--- order/views.py
+++ order/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import redirect
-from .models import OrderItem
-from django.views.generic import CreateView, TemplateView
+from .models import OrderItem, Order
+from django.views.generic import CreateView, TemplateView, ListView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .forms import OrderCreateForm
 from cart.cart import Cart
 
 
-class OrderCreateView(CreateView):
+class OrderCreateView(LoginRequiredMixin, CreateView):
     form_class = OrderCreateForm
     template_name = 'create_order.html'
+    login_url = '/account/login/'
 
     def get_context_data(self, **kwargs):
         context = super(OrderCreateView, self).get_context_data(**kwargs)
@@ -18,6 +20,7 @@ class OrderCreateView(CreateView):
         cart = Cart(request)
         form = self.form_class(request.POST)
         if form.is_valid():
+            form.instance.customer = self.request.user
             order = form.save()
             for item in cart:
                 OrderItem.objects.create(order=order,
@@ -28,6 +31,22 @@ class OrderCreateView(CreateView):
             return redirect('order:thanks')
         else:
             return self.form_invalid(form)
+
+
+class OrderListView(ListView):
+    model = Order
+    template_name = 'order_detail.html'
+    context_object_name = 'order_list'
+
+    def get_context_data(self, **kwargs):
+        context = super(OrderListView, self).get_context_data(**kwargs)
+        context["cart"] = Cart(self.request)
+        return context
+
+    def get_queryset(self):
+        queryset = super(OrderListView, self).get_queryset()
+        return queryset.filter(customer=self.request.user.id)
+
 
 class ThanksView(TemplateView):
     template_name = 'thanks.html'
