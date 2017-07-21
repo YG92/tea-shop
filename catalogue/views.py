6e323596799adeb4cd68a834fdcc494de3332a11
yulia Galiulina <julie_gal@mail.ru>
2017-07-21 20:38:01 +0500

    удалены подкатегории

--- catalogue/views.py
+++ catalogue/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import redirect
-from .models import Product, Category, Subcategory
+from .models import Product, Category
 from django.views.generic import ListView, CreateView
 from .forms import AddProductForm
 from cart.cart import Cart
@@ -9,18 +9,18 @@ class ProductListView(ListView):
 
     model = Product
     template_name = 'home.html'
-    subcat = None
+    cat = None
 
     def get_context_data(self, **kwargs):
         context = super(ProductListView, self).get_context_data(**kwargs)
         context["cats"] = Category.objects.all()
-        context["subcategory"] = self.subcat
+        context["category"] = self.cat
         context["cart"] = Cart(self.request)
         return context
 
     def get(self, request, *args, **kwargs):
-        if self.kwargs["subcat"]:
-            self.subcat = Subcategory.objects.get(pk=self.kwargs["subcat"])
+        if self.kwargs["cat"]:
+            self.cat = Category.objects.get(pk=self.kwargs["cat"])
         return super(ProductListView, self).get(request, *args, **kwargs)
 
     def dispatch(self, request, *args, **kwargs):
@@ -28,10 +28,10 @@ class ProductListView(ListView):
         return super(ProductListView, self).dispatch(request, *args, **kwargs)
 
     def get_queryset(self):
-        if self.kwargs["subcat"] == None:
-            queryset = Product.objects.all()
+        if self.kwargs["cat"] == None:
+            queryset = Product.objects.filter(category=1)
         else:
-            queryset = Product.objects.filter(subcategory=self.subcat)
+            queryset = Product.objects.filter(category=self.cat)
         if self.sort_field:
             queryset = queryset.order_by(self.sort_field)
         return queryset
@@ -41,7 +41,7 @@ class ProductListView(ListView):
         product_id = request.POST.get('product_id')
         product = Product.objects.get(pk=product_id)
         cart.add(product)
-        return redirect('home')
+        return redirect (request.META.get('HTTP_REFERER'))
 
 
 class AddProductsView(CreateView):
