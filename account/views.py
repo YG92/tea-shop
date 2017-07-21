f82e1e9b57846e3dbf4926b9fc9dbf99cbf67785
yulia Galiulina <julie_gal@mail.ru>
2017-07-21 20:12:59 +0500

    редирект на хомяк

--- account/views.py
+++ account/views.py
@@ -14,6 +14,6 @@ class RegisterFormView(FormView):
             new_user = authenticate(username=form.cleaned_data['username'],
                                     password=form.cleaned_data['password1'])
             login(self.request, new_user)
-            return redirect('/order/profile/')
+            return redirect('/product/')
         else:
             return self.form_invalid(form)
