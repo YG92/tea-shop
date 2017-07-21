2beaa7d7d7aadaa95aad1385c4bd4e4471b98b7d
yulia Galiulina <julie_gal@mail.ru>
2017-07-21 20:37:41 +0500

    дитэйл товара удален

--- catalogue/urls.py
+++ catalogue/urls.py
@@ -1,10 +1,8 @@
 from django.conf.urls import url
 from .import views
-from .views import ProductListView, AddProductsView, ProductDetailView
+from .views import ProductListView, AddProductsView
 
 urlpatterns = [
-url(r'^(?:(?P<subcat>\d+)/)?$', ProductListView.as_view(), name='home'),
+url(r'^(?:(?P<cat>\d+)/)?$', ProductListView.as_view(), name='home'),
 url(r'^add/', AddProductsView.as_view(), name='add-product'),
-url(r'^detail/(?P<pk>\d+)/$', ProductDetailView.as_view(),
-    name='product_detail'),
 ]
