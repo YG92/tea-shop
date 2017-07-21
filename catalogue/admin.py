ba4f1584e5996eaa92effa6b8bbec299bfd59fe0
yulia Galiulina <julie_gal@mail.ru>
2017-07-21 20:25:30 +0500

    удалены подкатегории

--- catalogue/admin.py
+++ catalogue/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from .models import Product, Category, Subcategory
+from .models import Product, Category
 
 admin.site.register(Category)
-admin.site.register(Subcategory)
 admin.site.register(Product)
