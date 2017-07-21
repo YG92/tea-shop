7f1b1cde5381fde684f02045046b90a82363ef72
yulia Galiulina <julie_gal@mail.ru>
2017-07-21 14:00:32 +0500

    формвью вместо криэйтвью

--- account/views.py
+++ account/views.py
@@ -1,18 +1,19 @@
 from django.shortcuts import redirect
-from django.views.generic import CreateView, TemplateView
+from django.views.generic import FormView
 from .forms import UserForm
 from django.contrib.auth import authenticate, login
 
 
-class RegisterFormView(CreateView):
+class RegisterFormView(FormView):
     form_class = UserForm
     template_name = 'register.html'
 
-    def post(self, request, *args, **kwargs):
-        form = self.form_class(request.POST)
+    def form_valid(self, form):
         if form.is_valid():
             form.save()
             new_user = authenticate(username=form.cleaned_data['username'],
                                     password=form.cleaned_data['password1'])
-        login(self.request, new_user)
-        return redirect('/order/profile/')
+            login(self.request, new_user)
+            return redirect('/order/profile/')
+        else:
+            return self.form_invalid(form)
