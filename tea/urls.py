599f93f725ecc9e57b988e544ed0f40e2311c48e
yulia <julie_gal@mail.ru>
2017-07-25 15:41:07 +0500

    ссылки

--- tea/urls.py
+++ tea/urls.py
@@ -2,11 +2,13 @@ from django.conf import settings
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.conf.urls.static import static
+from catalogue.views import ProductListView
 
 
 urlpatterns = [
-    url(r'^$', include('catalogue.urls')),
     url(r'^admin/', admin.site.urls),
+    url(r'^$', ProductListView.as_view(), name='home'),
+    url(r'^product/', include('catalogue.urls')),
     url(r'^account/', include('account.urls')),
     url(r'^cart/', include('cart.urls', namespace='cart')),
     url(r'^order/', include('order.urls', namespace='order')),
