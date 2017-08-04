05ed31d7a178cdcd3e526ff633c63d3d586a35e4
yulia <julie_gal@mail.ru>
2017-08-04 19:48:04 +0500

    функции всплываек вынесены из html

--- static_develop/js/modal.js
+++ static_develop/js/modal.js
@@ -1,9 +1,25 @@
-function showModal(parent){
-  var modal = parent.getElementsByClassName('product_detail')[0];
-  modal.style.display = "block";
-  var close = modal.getElementsByClassName('close')[0];
+var images = document.querySelectorAll('.image');
+var closeButtons = document.querySelectorAll('.close');
 
-  close.onclick = function(){
+function showModal(clickedElem){
+  return function(){
+    var parent = clickedElem.parentNode;
+    var modal = parent.querySelector('.product_detail');
+    modal.style.display = "block";
+  }
+}
+
+function hideModal(clickedElem){
+  return function(){
+    var modal = clickedElem.parentNode.parentNode;
     modal.style.display = "none";
   }
+}
+
+for (var i = 0; i < images.length; i++) {
+  images[i].addEventListener('click', showModal(images[i]));
+}
+
+for (var i = 0; i < closeButtons.length; i++) {
+  closeButtons[i].addEventListener('click', hideModal(closeButtons[i]));
 }
